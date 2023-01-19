0c586b82ce978bb18d5494c0c75e49a4af2b07b1
HiRurgLipay <sl38lipay@gmail.com>
2023-01-19 14:01:23 +0300

    Сделал 1,2 и 5 задание. Не разобрался с 4 заданием,не хватило времени,после сегодняшнего занятия доделаю

---
+++ lesson_5/homework.py
@@ -0,0 +1,44 @@
+import timeit
+import random
+
+# firstword = input('Enter a first word')
+# secondword = input('Enter a second word')
+#
+# # pin = f'!{secondword} {firstword}!'
+# # print(pin, sep='   ')
+#
+# pin = '!{0} {1}!'.format(secondword,firstword)
+# print(pin, sep='   ')
+'''Попрактиковался в форматировании строк'''
+
+
+# firstname = input('Enter your first name')
+# age = int(input('Enter your age'))
+# if age <= 0 or not int:
+#     print('Ошибка, повторите ввод')
+# elif age > 0 and age < 9:
+#     print('Привет,шкет', firstname,end='!')
+# elif age >= 10 and age < 18:
+#     print('Как жизнь', firstname,end='?')
+# elif age > 18 and age < 100:
+#     print('Чего желаете', firstname, end='?')
+# else:
+#     print(firstname,', вы лжете -  в наше время люди столько не живут ...')
+'''Сделал второе задание,попрактиковался с if,elif,else,and и тд..'''
+
+
+# a = 1
+# while a == 1:
+#     number = random.randrange(1, 10)
+#     personnumber = int(input('Введите целое число от 1 до 10 чтобы угадать\n'))
+#     if number == personnumber:
+#         print('Вы угадали!!! Cлучайно число было ', number)
+#     elif personnumber > number and personnumber < 10 or personnumber < number and personnumber > 10:
+#         print('Вы не угадали, случайно число было ', number)
+#     elif personnumber < 1 or personnumber > 10:
+#         print('Вы ввели неправильное число,попробуйте еще раз!')
+'''Создал программу в которой нужно будет угадывать число'''
+
+
+
+
