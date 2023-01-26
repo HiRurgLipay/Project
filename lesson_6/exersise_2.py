100112229e014e92386bf98ced75e7b93e2becd8
HiRurgLipay <sl38lipay@gmail.com>
2023-01-26 05:22:40 +0300

    Вывел факториал числа, так же попробовал использовать библиотку math

---
+++ lesson_6/exersise_2.py
@@ -0,0 +1,12 @@
+from math import factorial
+# n = int(input('Введите число n'))
+#
+# def factorial_(n):
+#     if n == 1:
+#         return n
+#     else:
+#         return n*factorial_(n-1)
+# print(factorial(n))
+
+n = int(input('Введите число n'))
+print(factorial(n))
